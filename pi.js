5b750d7e5d2272ca49fca1c67fb3b3de5d827f13
unknown <denaroc@MOBCTEK128.yoox.net>
2014-03-06 09:34:29 +0100

    append

--- pi.js
+++ pi.js
@@ -288,6 +288,28 @@ pi.storage = (function () {
 }());
 
 /**
+ * @param {String} elm elm to append
+ * @param {String} target target to append
+ * @param {String} pos top|down|append
+ */
+pi.append = function (elm, target, pos) {
+	"use strict";
+	elm = pi(elm);
+	target = pi(target);
+	switch (pos) {
+	case 'top':
+		target.parentNode.insertBefore(elm, target);
+		break;
+	case 'down':
+		target.parentNode.insertBefore(elm, target.nextSibling);
+		break;
+	default:
+		target.appendChild(elm);
+		break;
+	}
+};
+
+/**
  * @param {Object} 
  * @param {String} params.type
  * @param {String} params.url
