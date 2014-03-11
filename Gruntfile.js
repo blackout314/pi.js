09bbbf9db677712143a7f4a1a1ac54a76cd5c4b2
blackout314 <blackout314@vm-0.blackout314.koding.kd.io>
2014-03-11 12:45:32 +0000

    banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,55 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: '<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
-      }
-    },
-    replace: {
-        dist: {
-            options: {
-                prefix:'',
-                patterns: [
-                    {
-                        match: '/@@VERSION_NUMBER/g',
-                        replacement: '<%= pkg.version %>',
-                        expression: true
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+          options: {
+            banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+          },
+          build: {
+            src: '<%= pkg.name %>.js',
+            dest: 'build/<%= pkg.name %>.min.js'
+          }
+        },
+        replace: {
+            dist: {
+                options: {
+                    prefix:'',
+                    patterns: [
+                        {
+                            match: '/@@VERSION_NUMBER/g',
+                            replacement: '<%= pkg.version %>',
+                            expression: true
+                        }
+                    ]
+                },
+                files: [
+                    { 
+                        expand: true, 
+                        flatten: true, 
+                        src: ['build/<%= pkg.name %>.min.js'], 
+                        dest: 'build/' 
                     }
                 ]
-            },
-            files: [
-                { 
-                    expand: true, 
-                    flatten: true, 
-                    src: ['build/<%= pkg.name %>.min.js'], 
-                    dest: 'build/' 
-                }
-            ]
-      }
-    }
-  });
+          }
+        }
+    });
+      
+    grunt.registerTask('banner', function() {
+        grunt.log.writeln(' ' );
+        grunt.log.writeln('       ____');
+        grunt.log.writeln('   _||__|  |    ______   ________');
+        grunt.log.writeln('  (        | |      | |      |');
+        grunt.log.writeln('  /-()---() ~ ()--() ~ ()--()');
+        grunt.log.writeln(' ' );
+    });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-replace');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify','replace']);
+  grunt.registerTask('default', ['banner','uglify','replace']);
 
 };
