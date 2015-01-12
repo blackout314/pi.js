5d0a78151b974768e1debe5599803e4073373388
blackout314 <blackout_lair@yahoo.com>
2015-01-12 17:08:03 +0100

    add tests to pub sub topics

---
+++ tests/pubsub.js
@@ -0,0 +1,22 @@
+module("tests", {
+	setup: function () {
+	},
+	teardown: function () {
+	}
+});
+
+test('pi.T : topics test ok', function(){
+	ok( typeof(pi.T) === 'object' );
+});
+
+test('pi.T topics on', function(){
+	var global = 0;
+	var callback = function(a){ console.log('LOG: '+a); global = global + 1;};
+	pi.T.sub('NOTICE', callback);
+	pi.T.pub('NOTICE',['hello']);
+	ok( global === 1 );
+
+	pi.T.unsub({'topic':'NOTICE','callback':callback});
+	pi.T.pub('NOTICE',['hello']);
+	ok( global === 1 );
+});
