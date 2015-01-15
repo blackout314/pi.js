0558e546d228ec088ff2e951e60951412cd8eef8
blackout314 <blackout_lair@yahoo.com>
2015-01-15 14:30:26 +0100

    add tests and raise coverage

--- tests/dom.js
+++ tests/dom.js
@@ -11,13 +11,41 @@ test('dom test', function(){
 	ok( typeof(pi.classAdd) === 'function' );
 });
 
-test('dom classAdd', function(){
+test('dom classAdd classDel classHas', function(){
 	var test = pi('#test');
 	pi.classAdd(test,'prova');
 	ok( pi.classHas(test,'prova') === true );
+
+	pi.classAdd('#test','prova2');
+	ok( pi.classHas('#test','prova2') === true );
+
+	pi.classDel(test,'prova');
+	pi.classDel('#test','prova2');
+	ok( pi.classHas(test,'prova') === false );
+	ok( pi.classHas('#test','prova2') === false );
+});
+
+test('dom classToggle', function(){
+	var test = pi('#test');
+	pi.classToggle(test,'prova');
+	ok( pi.classHas(test,'prova') === true );
+
+	pi.classToggle('#test','prova');
+	ok( pi.classHas('#test','prova') === false );
 });
 
 test('dom H remove', function(){
 	pi.H.remove('#d1','#test');
 	ok( pi('#d1') === null );
+});
+
+test('dom classAdd classDel classToggle forEach', function(){
+	pii.classAdd('.o', 'prova');
+	ok( pi.classHas('#d1','prova') === true );
+
+	pii.classDel('.o', 'prova');
+	ok( pi.classHas('#d1','prova') === false );
+
+	pii.classToggle('.o', 'prova');
+	ok( pi.classHas('#d1','prova') === true );
 });
