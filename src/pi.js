dee0600a7d88ff756c1c6edbc0ef299a5d0cbfbe
blackout314 <blackout_lair@yahoo.com>
2015-05-27 17:39:30 +0200

    add mock up tracking GA

--- src/pi.js
+++ src/pi.js
@@ -83,7 +83,22 @@ pi.C = {
 		"addEventListener" : !!window.addEventListener,                   // eventListener
 		"querySelectorAll" : !!document.querySelectorAll,                 // querySelector
 		"classList" : !!document.documentElement.classList                // classList
-	}
+	},
+    "UA" : "XXXXX"
+};
+
+// --------- Tracking Function
+
+pi.track = function () {
+    /*
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', pi.C.UA, 'auto');
+    ga('send', 'pageview');
+    */
+    return true;
 };
 
 // -------- Debug
