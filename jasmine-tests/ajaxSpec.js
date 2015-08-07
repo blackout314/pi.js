f04bce5466ac9bafbfb789c5571de64735ca042a
blackout314 <blackout_lair@yahoo.com>
2015-08-07 16:23:07 +0200

    ajaxspec

---
+++ jasmine-tests/ajaxSpec.js
@@ -0,0 +1,32 @@
+describe("Ajax Functions", function() {
+
+  beforeEach(function() {
+    var server = sinon.fakeServer.create();
+  	server.respondWith("GET", "example.json", [
+  		200,
+  		{"Content-Type": "application/json"},
+  		'[{"id": 0, "tweet": "Hello World"}]'
+  		]
+  	);
+
+  });
+  afterEach(function() {
+    server.restore();
+  });
+
+  it("i try to load example.json", function() {
+    var checkData = {};
+
+    var f = function (data) {
+  		checkData = JSON.parse(data);
+  	};
+  	pi.A({
+  		type:'GET',
+  		url:'example.json',
+  		success:f
+  	});
+    server.respond();
+
+    expect( checkData[0].tweet ).toBe("Hello World");
+  });
+});
