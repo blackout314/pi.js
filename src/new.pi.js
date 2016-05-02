589aa5a7539d3ef06d653985660e6bc358bef6c9
blackout314 <blackout_lair@yahoo.com>
2016-05-02 12:27:32 +0200

    new addclass on element

--- src/new.pi.js
+++ src/new.pi.js
@@ -39,6 +39,37 @@ Element.prototype.on = Element.prototype.addEventListener;
  */
 Element.prototype.rm = Element.prototype.removeEventListener;
 
+Element.prototype.addClass = function (c) {
+  "use strict";
+	this.classList.add(c);
+}
+
+Element.prototype.delClass = function (c) {
+  "use strict";
+	this.classList.remove(c);
+}
+
+NodeList.prototype.cycle = function (event, fn, action) {
+    "use strict";
+	var i = 0;
+	for (i = 0; i < this.length; i++) {
+		this.item(i)[action](event, fn, false);
+	}
+};
+/**
+ * @example pii('.class').on('click', callback)
+ */
+NodeList.prototype.on = function (event, fn) {
+    "use strict";
+	NodeList.prototype.cycle.call(this, event, fn, 'addEventListener');
+};
+/**
+ * @example pii('.class').rm('click', callback)
+ */
+NodeList.prototype.rm = function (event, fn) {
+	"use strict";
+    NodeList.prototype.cycle.call(this, event, fn, 'removeEventListener');
+};
 
 
 
